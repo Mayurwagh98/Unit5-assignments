21d5c3f98cc2248d1b13561963e05dac4f46115a
Mayurwagh98 <mswagh1998@gmail.com>
2022-04-29 04:41:42 +0530

    react-google-timer

---
+++ Day9 (froms and refs)/react-google-timer/src/App.jsx
@@ -0,0 +1,14 @@
+import Counter from './components/Counter';
+import Timer from './components/Timer';
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<Counter />
+			<Timer />
+		</div>
+	);
+}
+
+export default App;
---
+++ Day9 (froms and refs)/react-google-timer/src/components/Counter.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect, useRef } from 'react';
+
+const Counter = () => {
+	const [second, setSecond] = useState(0);
+	const [milliSecond, setMilliSecond] = useState(0);
+	const [act, setAct] = useState(false);
+	const secRef = useRef(null);
+	const milRef = useRef(null);
+
+	useEffect(() => {
+		if (act) {
+			secStart();
+			milStart();
+		}
+
+		return () => {
+			handleClear();
+		};
+	}, []);
+
+	const secStart = () => {
+		secRef.current = setInterval(() => {
+			setSecond(prev => prev + 1);
+		}, 60000);
+	};
+
+	const milStart = () => {
+		milRef.current = setInterval(() => {
+			setMilliSecond(prev => {
+				if (prev >= 60) {
+					return 0;
+				}
+				return prev + 1;
+			});
+		}, 1000);
+	};
+
+	const handleClear = () => {
+		clearInterval(secRef.current);
+		clearInterval(milRef.current);
+	};
+
+	return (
+		<div className='stop_watch'>
+			<h1>Stop Watch</h1>
+
+			<input
+				type='Number'
+				id='second'
+				placeholder='enter seconds'
+				onChange={e => setSecond(Number(e.target.value))}
+			/>
+			<h1>
+				<span className='sec'>{second}</span>m
+				<span className='mil'> {milliSecond}s</span>
+			</h1>
+			<div className='buttons'>
+				<button
+					onClick={() => {
+						handleClear();
+					}}
+				>
+					stop
+				</button>
+				<button
+					onClick={() => {
+						secStart();
+						milStart();
+						setAct(!act);
+					}}
+				>
+					start
+				</button>
+				<button
+					onClick={() => {
+						handleClear();
+						setSecond(0);
+						setMilliSecond(0);
+					}}
+				>
+					reset
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Counter;
